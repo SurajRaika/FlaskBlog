62e88262172f623832d5a407fb7b052d9bf6f633
SurajRaika <103476230+SurajRaika@users.noreply.github.com>
2022-05-25 13:34:54 +0530

    Second
    Optimised for Form , adding form_lable and form_input are left in Html

---
+++ form.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField , EmailField ,PasswordField ,BooleanField , SubmitField
+from wtforms.validators import DataRequired , Length , Email , EqualTo
+
+
+
+class RegistrationForm(FlaskForm):
+    username=StringField('Username',validators=[DataRequired(),Length(min=5,max=20)])
+    email=EmailField('Email Address',validators=[DataRequired(),Length(min=4,max=20) ,Email()])
+    password=PasswordField('Password',validators=[DataRequired(),Length(min=4)])
+    confir_password=PasswordField('Password',validators=[DataRequired(),Length(min=4) ,EqualTo('password') ])
+    submit = SubmitField('Sign up')	
+
+
+class LoginForm(FlaskForm):
+    username=StringField('Username',validators=[DataRequired(),Length(min=5,max=20)])
+    email=EmailField('Email Address',validators=[DataRequired(),Length(min=4,max=20) ,Email()])
+    password=PasswordField('Password',validators=[DataRequired(),Length(min=4)])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Login Form')	
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from flask import Flask , render_template
-
+from form import RegistrationForm
 app=Flask(__name__)
 
 data = [{'author':'Suraj','title':'Not What you think in machine learning','content':'As we know guys that today everyyyyyyyy one is waitingggggg for some ......','date_posted':'April 20 2020'},{'author':'Ravi','title':'Running a love stry ','content':'so story begin with ','date_posted':'march 20 2020'}]
@@ -9,9 +9,18 @@ data = [{'author':'Suraj','title':'Not What you think in machine learning','cont
 def home():
     return render_template("home.html",posts=data)
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 @app.route("/login")
 def login():
     return render_template("login.html" )
+
+@app.route("/registration")
+@app.route("/signin")
+def registration():
+    return render_template("registration.html" )
 
 if __name__=="__main__":
     app.run(debug=True)
