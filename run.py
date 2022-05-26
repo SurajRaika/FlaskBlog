d5f9038bc0818ff0d10f3f93f82c8d37ed1c1276
SurajRaika <103476230+SurajRaika@users.noreply.github.com>
2022-05-27 05:07:31 +0530

    Form_Validation2
    added some more post but still we have to arrange them

--- run.py
+++ run.py
@@ -5,7 +5,10 @@ from form import RegistrationForm , LoginForm
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = '1677626b7f3074acc0f04c9fdf6s145a'
-data = [{'author':'Suraj','title':'Not What you think in machine learning','content':'As we know guys that today everyyyyyyyy one is waitingggggg for some ......','date_posted':'April 20 2020'},{'author':'Ravi','title':'Running a love stry ','content':'so story begin with ','date_posted':'march 20 2020'}]
+data = [
+    {'author':'Suraj','title':'Not What you think in machine learning','content':'As we know guys that today everyyyyyyyy one is waitingggggg for some ......','date_posted':'April 20 2020'}
+    ,{'author':'Ravi','title':'Lorem, ipsum dolor.','content':'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, dolorum.','date_posted':'march 20 2020'}
+    ,{'author':'John Wick','title':'Ullam esse mollitia.','content':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam esse mollitia, placeat rem natus molestias aliquid odio dicta sequi nisi optio sapiente quos.','date_posted':'march 20 2020'}]
 
 @app.route("/")
 @app.route("/home")
