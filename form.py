8616a2c39e98caffd9f963188c1064025b341d33
SurajRaika <103476230+SurajRaika@users.noreply.github.com>
2022-05-26 18:26:27 +0530

    3rd

--- form.py
+++ form.py
@@ -8,7 +8,7 @@ class RegistrationForm(FlaskForm):
     username=StringField('Username',validators=[DataRequired(),Length(min=5,max=20)])
     email=EmailField('Email Address',validators=[DataRequired(),Length(min=4,max=20) ,Email()])
     password=PasswordField('Password',validators=[DataRequired(),Length(min=4)])
-    confir_password=PasswordField('Password',validators=[DataRequired(),Length(min=4) ,EqualTo('password') ])
+    confirm_password=PasswordField('Password',validators=[DataRequired(),Length(min=4) ,EqualTo('password') ])
     submit = SubmitField('Sign up')	
 
 
--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-from flask import Flask , render_template
+from flask import Flask , render_template ,flash
 from form import RegistrationForm
 app=Flask(__name__)
-
+app.config['SECRET_KEY'] = '1677626b7f3074acc0f04c9fdf62145a'
 data = [{'author':'Suraj','title':'Not What you think in machine learning','content':'As we know guys that today everyyyyyyyy one is waitingggggg for some ......','date_posted':'April 20 2020'},{'author':'Ravi','title':'Running a love stry ','content':'so story begin with ','date_posted':'march 20 2020'}]
 
 @app.route("/")
@@ -17,10 +17,15 @@ def about():
 def login():
     return render_template("login.html" )
 
-@app.route("/registration")
-@app.route("/signin")
+@app.route("/registration",methods=['POST',"GET"])
 def registration():
-    return render_template("registration.html" )
+    form=RegistrationForm()
+    if form.email.data == 'suajraika5sr@gmail.com' and form.password.data == "suajraika5sr@gmail.com":
+            flash(f' Logged In ','success')
+            return redirect(url_for("home")) 
+    else :
+            flash(f'Wrong Password or email ','danger')
+    return render_template("registration.html",form = form )
 
 if __name__=="__main__":
     app.run(debug=True)
