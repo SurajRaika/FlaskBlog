611445103f8435c3a2af13a98a423e9b6f59c49a
SurajRaika <103476230+SurajRaika@users.noreply.github.com>
2022-06-05 16:34:18 +0530

    blogFlask4.4
    Now able to delete post in next only i have to add modal to confirm user decisions

--- routes.py
+++ routes.py
@@ -186,11 +186,12 @@ def post(post_id):
 
 
 
-# @app.route("/post/<int:post_id>/update")
-# def update_post(post_id):
-#     # post=Post.query.get(post_id)
-#     # let use another 
-#     post=Post.query.get_or_404(post_id)
-#     if not post.author == current_user:
-#         abort(403)
-#     return render_template('post.html',title=post.title , post=post)
+@app.route("/post/<int:post_id>/Delete")
+def Delete(post_id):
+    # let use another 
+    post=Post.query.get_or_404(post_id)
+    if not post.author == current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('home'))
